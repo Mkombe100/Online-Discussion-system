c00d173f701c3e8138bb33d3aad0585eca9bd23c
Muksini Mkombe <muksinimkombe23@gmail.com>
2026-06-19 04:29:55 +0300

    Update room.js

--- public/room.js
+++ public/room.js
@@ -175,12 +175,30 @@ function createVideo(id) {
   video.autoplay = true;
   video.playsInline = true;
   video.className = "part-videos";
-  video.id = id;    i need in  phone view when user double click then video comes in autorotate 
+  video.id = id;
 
-  // Click participant video to expand
-  video.addEventListener("click", () => {
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
+  // Double click to fullscreen + landscape
+  video.addEventListener("dblclick", async () => {
+    try {
+      if (video.requestFullscreen) {
+        await video.requestFullscreen();
+      }
+
+      // Rotate to landscape on supported phones
+      if (screen.orientation && screen.orientation.lock) {
+        await screen.orientation.lock("landscape");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // Unlock orientation when exiting fullscreen
+  document.addEventListener("fullscreenchange", () => {
+    if (!document.fullscreenElement) {
+      if (screen.orientation && screen.orientation.unlock) {
+        screen.orientation.unlock();
+      }
     }
   });
 
