0af225994efcf0b9c4189e0007e8c5ae86f3b8b5
Muksini Mkombe <muksinimkombe23@gmail.com>
2026-07-06 11:05:09 +0300

    Modify room creation to redirect instead of respond
    Redirect to room URL after creating a room.

--- routes/createRoom.js
+++ routes/createRoom.js
@@ -2,17 +2,8 @@ const router = require('express').Router();
 
 router.post("/createRoom", async (req, res) => {
   const roomCode = Math.floor(10000 + Math.random() * 90000);
-  
-  res.json({
-    success: true,
-    roomCode: roomCode,
-    roomUrl: `/room.html?room=${roomCode}`
-  });
-});
 
-router.get("/room.html", (req, res) => {
-  const path = require('path');
-  res.sendFile(path.join(__dirname, "..", "public", "room.html"));
+  res.redirect(`/room.html?room=${roomCode}`);
 });
 
 module.exports = router;
